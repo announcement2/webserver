ff5491a170aef6c2c0e039cd3986ef4f838b1466
LukeG007 <luke@gilsinger.net>
2022-12-18 16:20:20 -0500

    Add kiosk mode

--- main.py
+++ main.py
@@ -59,5 +59,36 @@ def make_announcement():
     
     return render_template('make_announcement.html', title='Make Announcement', cookies=cookies, presets=announcement_presets)
 
+@app.route('/kiosk')
+def kiosk_home():
+    index = 0
+    delete_announcements = []
+    for announcement in app.recent_announcements:
+        current_time = time.time()
+        time_since_announcement = current_time-announcement['time_sent']
+        if time_since_announcement >= 3600:
+            delete_announcements.append(index)
+
+    for announcement in delete_announcements:
+        app.recent_announcements.pop(announcement)
+    
+    return render_template('kiosk_home.html', title='Home', announcements=app.recent_announcements, current_time=time.time(), naturaltime=humanize.naturaltime)
+
+@app.route('/kiosk/make_announcement', methods=['GET', 'POST'])
+def kiosk_make_announcement():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        announcement_client.announce(message, name)
+        resp = redirect('/kiosk')
+        resp.set_cookie('name', name)
+        return resp
+
+    cookies = dict(request.cookies)
+    if not 'name' in cookies:
+        cookies['name'] = ''
+    
+    return render_template('kiosk_make_announcement.html', title='Make Announcement', cookies=cookies, presets=announcement_presets)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4592)
